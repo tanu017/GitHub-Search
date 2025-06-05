deb20844d0ad7c66c6d7ef0daf367e6ebff608cf
Tanushree <122681103+tanu017@users.noreply.github.com>
2025-06-05 20:07:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,22 @@ const searchBtn = document.querySelector(".search-button");
 searchBtn.addEventListener('click', ()=> {
     getUserDetails(getUsername.value);
 })
+
 getUsername.addEventListener("keypress", function(event) {
     if (event.key === "Enter") { 
         getUserDetails(getUsername.value); 
     }
 });
+
 const getUserDetails = async(username) => {
+
     let url = `https://api.github.com/users/${username}`;
 
-    const response = await fetch(url);
+    const response = await fetch(url, {
+        headers: {
+            "Authorization": "GITHUB-SEARCH_TOKEN"
+        }
+    })
 
     const data = await response.json();
 
@@ -21,7 +28,8 @@ const getUserDetails = async(username) => {
 
     githubDetails.innerHTML = `<div id="name-image"><h2>Name : ${data.name}</h2>
                                 <img src="${data.avatar_url}"></div>
-                                <div id="details"><p>${data.bio}</p>
+                                <div id="details">
+                                <p> <b>Bio: </b>${data.bio}</p>
                                 <p> <b>Username: </b>${data.login}</p>
                                 <p> <b>Location: </b>${data.location}</p>
                                 <p> <b>Company Name: </b>${data.company}</p>
@@ -29,3 +37,19 @@ const getUserDetails = async(username) => {
     `
 }
 getUserDetails()
+
+function light() {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+    document.getElementById("github-details").style.border = "2px double black";
+    document.getElementById("details").style.border = "2px double black";
+    document.getElementById("name-image").style.border = "2px double black";
+}
+
+function darkmode() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+    document.getElementById("github-details").style.border = "2px double white";
+    document.getElementById("details").style.border = "2px double white";
+    document.getElementById("name-image").style.border = "2px double white";
+}

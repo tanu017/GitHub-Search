38a6afa3b3ca0dbe580d9d5e6cd27b69fc340e7f
Tanushree <122681103+tanu017@users.noreply.github.com>
2025-06-05 18:33:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ const searchBtn = document.querySelector(".search-button");
 searchBtn.addEventListener('click', ()=> {
     getUserDetails(getUsername.value);
 })
+getUsername.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") { 
+        getUserDetails(getUsername.value); 
+    }
+});
 const getUserDetails = async(username) => {
     let url = `https://api.github.com/users/${username}`;
 
@@ -14,12 +19,13 @@ const getUserDetails = async(username) => {
 
     let githubDetails = document.querySelector("#github-details");
 
-    githubDetails.innerHTML = `<h2>Name : ${data.name}</h2>
-                                <img src="${data.avatar_url}">
-                                <p>${data.bio}</p>
-                                <p> Username: ${data.login}</p>
-                                <p> Location: ${data.location}</p>
-                                <p> Company Name: ${data.company}</p>
+    githubDetails.innerHTML = `<div id="name-image"><h2>Name : ${data.name}</h2>
+                                <img src="${data.avatar_url}"></div>
+                                <div id="details"><p>${data.bio}</p>
+                                <p> <b>Username: </b>${data.login}</p>
+                                <p> <b>Location: </b>${data.location}</p>
+                                <p> <b>Company Name: </b>${data.company}</p>
+                                <div>
     `
 }
 getUserDetails()
